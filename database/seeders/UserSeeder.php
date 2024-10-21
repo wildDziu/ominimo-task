dc1c1e601778a520c9a48fd508466838dd056f5b
dziulatex <paweldziurasoftware@yahoo.com>
2024-10-21 21:33:33 +0200

    docker-compose ready for setting up project from docker compose up -d, and rest changes needed for project to work in seed mode.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,19 +12,7 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        // Create admin user
-        $adminUser = User::create([
-            'name' => 'Admin User',
-            'email' => 'admin@example.com',
-            'password' => Hash::make('adminadmin321'),
-        ]);
-
-        // Attach admin role to admin user
-        $adminRole = Role::where('name', RoleEnum::ADMIN)->first();
-        $adminUser->roles()->attach($adminRole);
-
-        // Create regular users
-        User::factory(9)->create()->each(function ($user) {
+        User::factory(10)->create()->each(function ($user) {
             $userRole = Role::where('name', RoleEnum::USER)->first();
             $user->roles()->attach($userRole);
         });
---
+++ docker/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+composer install
+# Check if the database exists and run migrations if necessary
+if ! mysql -h mysql -u"${DB_USERNAME}" -p"${DB_PASSWORD}" -e "use ${DB_DATABASE};" > /dev/null 2>&1; then
+  echo "Database does not exist. Running migrations and seeding..."
+  php artisan migrate --seed
+else
+  echo "Database exists. Skipping migrations and seeding."
+fi
+
+# Check if APP_ENV is set to 'local'
+if [ "${APP_ENV}" = "local" ]; then
+  echo "Running in local environment, starting npm in development mode..."
+  npm run dev &
+else
+  echo "Running in non-local environment, building assets for production..."
+  npm run build
+fi
+
+# Start supervisord to keep the container running and manage the services
+exec supervisord -c /etc/supervisor/supervisord.conf
